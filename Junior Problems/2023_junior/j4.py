8f0cb114f27a3070e51751ae9b2f3244c2f1117f
Alberto4944 <albertwu040@gmail.com>
2025-06-12 13:02:57 -0600

    2023 j4 completed

--- Junior Problems/2023_junior/j4.py
+++ Junior Problems/2023_junior/j4.py
@@ -1,17 +1,26 @@
-collumns = 5#int(input())
+# Completed on 6/12/2025
 
-row1 = "1 0 1 0 1".split()#input().split()
-row2 = "0 0 0 0 0".split()#input().split()
+collumns = int(input())
+row1 = input().split()
+row2 = input().split()
 
 tape = (row1.count("1") + row2.count("1")) * 3
+          
+def check_left_right(row, triangle, tape):
+    if row[triangle-1] == "1" and triangle > 0:
+        tape-=1
+    if triangle < (collumns-1) and row[triangle+1] == "1":
+        tape-=1
+    return tape          
 
-
-
-for triangle in range(len(row1)):
+for triangle in range(collumns):
     if row1[triangle] == "1":
-        if row1[triangle+1] == "1" and triangle < len(row1)-1 or row1[triangle-1] == "1" and triangle > 0 == "1" or row2[triangle] == "1":
+        if triangle % 2 == 0 and row2[triangle] == "1":
             tape-=1
-
+        tape = check_left_right(row1, triangle, tape)
+    if row2[triangle] == "1":
+        if triangle % 2 == 0 and row1[triangle] == "1":
+            tape-=1
+        tape = check_left_right(row2, triangle, tape)
+        
 print(tape)
-
-print(row1, row2)
