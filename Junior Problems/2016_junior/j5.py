8f45cc853555a7fadb3bdf87d64d4c6e120766f6
Alberto4944 <albertwu040@gmail.com>
2025-06-13 19:01:46 -0600

    tried 2016 j5, not completely working

--- Junior Problems/2016_junior/j5.py
+++ Junior Problems/2016_junior/j5.py
@@ -7,25 +7,14 @@ country1.sort()
 country2.sort()
 
 def find_maximum(country1, country2):
-    country1_2 = country1
-    country2_2 = country2
     output1 = 0
     for i in range(people):
-        lowest1 = int(country1.pop(0))
-        highest2 = int(country2.pop(-1))
+        lowest1 = int(country2.pop(0))
+        highest2 = int(country1.pop(-1))
         output1+=max(lowest1,highest2)
         
-    output2 = 0
-    for i in range(people):
-        highest1 = int(country1_2.pop(0))
-        lowest2 = int(country2_2.pop(-1))
-        output2+=max(highest1,lowest2)
         
-    if output1 > output2:
-        return output1
-    else:
-        return output2
-    
+    return output1
     
 def find_minimum(country1, country2):
     output = 0
